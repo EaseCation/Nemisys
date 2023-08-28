922b46c1e6bf788d6b7de53756ce0a8034865416
Woder <17339354+wode490390@users.noreply.github.com>
2023-08-28 22:05:56 +0800

    修复上报的一些异常

---
+++ src/main/java/org/itxtech/nemisys/utils/StacklessException.java
@@ -0,0 +1,23 @@
+package org.itxtech.nemisys.utils;
+
+public class StacklessException extends Exception {
+    public StacklessException() {
+        super(null, null, true, false);
+    }
+
+    public StacklessException(String message) {
+        super(message, null, true, false);
+    }
+
+    public StacklessException(String message, Throwable cause) {
+        super(message, cause, true, false);
+    }
+
+    public StacklessException(Throwable cause) {
+        super(null, cause, true, false);
+    }
+
+    protected StacklessException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
---
+++ src/main/java/org/itxtech/nemisys/utils/StacklessRuntimeException.java
@@ -0,0 +1,23 @@
+package org.itxtech.nemisys.utils;
+
+public class StacklessRuntimeException extends RuntimeException {
+    public StacklessRuntimeException() {
+        super(null, null, true, false);
+    }
+
+    public StacklessRuntimeException(String message) {
+        super(message, null, true, false);
+    }
+
+    public StacklessRuntimeException(String message, Throwable cause) {
+        super(message, cause, true, false);
+    }
+
+    public StacklessRuntimeException(Throwable cause) {
+        super(null, cause, true, false);
+    }
+
+    protected StacklessRuntimeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
